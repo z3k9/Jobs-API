cdad2521bcfdee197f44ee725dafc62800760592
z3k9 <ajelelade@gmail.com>
2023-02-03 19:31:33 +0100

    completed the jobs controller set up and added security packages to the app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const app = express();
 const notFound = require('./services/notfound');
 const v1 = require('./routes/v1');
 require('dotenv').config();
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100 // Rate limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.get('/', (req,res)=>{
     res.send('Jobs API');
 });
--- src/routes/jobs/jobs.controller.js
+++ src/routes/jobs/jobs.controller.js
@@ -1,26 +1,56 @@
 const Job = require('../../models/job.mongo');
 
+
 async function getAllJobs(req,res){
-    res.send('All Jobs');
+    const jobs = await Job.find({createdBy: req.user._id}).sort('createdAt');
+    res.status(200).json({jobs, count:jobs.length});
 }
 
 async function getAJob(req,res){
-    res.send('A job');
+    const {
+        user: {_id}, 
+        params:{id:jobId}
+    } = req;
+    const job = await Job.findOne({
+        _id: jobId, createdBy: _id
+    });
+    if(!job){
+        res.status(404).json({msg: 'This job does not exist'});
+    }
+    res.status(200).json({job});
 }
 
 async function createAJob(req,res){
     req.body.createdBy = req.user._id;
     const job = await Job.create(req.body);
     
-    res.status(200).json(job);
+    res.status(200).json({job});
 }
 
 async function updateAJob(req,res){
-    res.send('Job updated');
+    const {
+        body: {company, position},
+        user: {_id},
+        params: {id: jobId}
+    } = req;
+    if(company === "" || position === ""){
+        return res.status(401).json({msg: 'Company or position fields cannot be empty'});
+    }
+    const job = await Job.findByIdAndUpdate({_id:jobId, createdBy: _id}, req.body, {new:true, runValidators:true});
+    if(!job){
+        return res.status(404).json({msg: `No job with id ${jobId}`});
+    }
+    res.status(201).json({job});
+    console.log(req);
 }
 
 async function deleteAJob(req,res){
-    res.send('Job deleted');
+    const { user: {_id}, params:{id:jobId}} = req;
+    const job = await Job.findOneAndRemove({_id: jobId, createdBy: _id});
+    if(!job){
+        return res.status(404).json({msg: 'Job does not exist'});
+    }
+    res.status(201).json({msg: 'Job deleted successfully'});
 }
 
 module.exports = { getAllJobs, getAJob, createAJob, updateAJob, deleteAJob }
